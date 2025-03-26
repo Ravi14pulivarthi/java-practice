3d52272c48e29c6f273701fb9f05805466311b66
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2025-03-26 22:58:43 +0530

    tables

--- Javapratice/Condection.java
+++ Javapratice/Condection.java
@@ -1,21 +1,37 @@
 public class Condection {
     public static void main(String a[]) {
 
-        int i = 1;
+        // int i = 1;
 
-        while (i <= 4) {
-            System.out.println("Hii Ravi" + i);
-            int num2 = 3;
-            for (int j = 1; j <= num2; j++) {
-                System.out.println("Extra " + j);
-            }
-            i++;
-        }
+        // while (i <= 4) {
+        //     System.out.println("Hii Ravi" + i);
+        //     int num2 = 3;
+        //     for (int j = 1; j <= num2; j++) {
+        //         System.out.println("Extra " + j);
+        //     }
+        //     i++;
+        // }
 
         // int num = 10;
         // for (int j = 1; j <= num; j++) {
         // System.out.println("For loop" + j);
         // }
+
+
+
+
+
+          int num1=10;
+           int num2=10;
+
+            for(int i=1 ; i<=num1;i++){
+
+                 for( int j=1;j<=num2;j++){
+
+                     System.out.println( i + "*" + j + "=" +(i*j));
+
+                 }
+            }
     }
 
 }

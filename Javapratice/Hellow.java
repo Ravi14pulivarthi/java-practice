8b2f69dc62af169918a3790d3a310d792e147d00
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2025-03-26 07:29:36 +0530

    varables in java

--- Javapratice/Hellow.java
+++ Javapratice/Hellow.java
@@ -4,6 +4,11 @@
     public static void main(String a[])
     {
         
-    System.out.println("Ravi");
+         int num1 = 2;
+          int num2=4;
+
+           String name="Ravikumar";
+    System.out.println(num1+num2);
+    System.out.println(num1+name);
     }
 }

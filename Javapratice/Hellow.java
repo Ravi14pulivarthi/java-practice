e2c3795c73a21f95012d74a687551d420d9d8ae0
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2025-03-26 07:34:17 +0530

    java double varables

--- Javapratice/Hellow.java
+++ Javapratice/Hellow.java
@@ -8,7 +8,11 @@
           int num2=4;
 
            String name="Ravikumar";
+
+            double num3= 2.3;
     System.out.println(num1+num2);
     System.out.println(num1+name);
+    System.out.println(num3+num1);
+
     }
 }

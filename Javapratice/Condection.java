5ed7264a248c4f1c084a128133016a8543587903
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2025-03-26 22:37:19 +0530

    for loop in java

--- Javapratice/Condection.java
+++ Javapratice/Condection.java
@@ -4,10 +4,13 @@ public class Condection {
         int i = 1;
 
         while (i <= 4) {
-
             System.out.println("Hii Ravi" + i);
             i++;
+        }
 
+        int num = 10;
+        for (int j = 1; j <= num; j++) {
+            System.out.println("For loop" + j);
         }
     }
 

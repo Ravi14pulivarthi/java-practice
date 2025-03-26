7214a1a122872006a1d5dbd22cec7f1a1f535c5e
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2025-03-26 13:13:58 +0530

    if else condection in java

--- Javapratice/Newhello.java
+++ Javapratice/Newhello.java
@@ -2,11 +2,18 @@ public class Newhello {
     
      public static void main( String a[]){
 
-        byte num1=120;
-
-         int num2=num1;
+         int  num1=120;
+          int num2=100;
          
         System.out.println("NewHeelow World");
-         System.out.println(num2);
+         System.out.println(num2+num1);
+            int x=10;
+
+             if(x>20){
+                 System.out.println("Heloo world");
+             }
+              else{
+                 System.out.println("Not Hellow");
+              }
      }
 }

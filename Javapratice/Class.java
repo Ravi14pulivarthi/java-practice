ce0bb7ed50e5f935940edef841414caa6e3d5ce7
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2025-03-27 22:07:35 +0530

    multydiminisation array

--- Javapratice/Class.java
+++ Javapratice/Class.java
@@ -1,31 +1,30 @@
+class Calculater {
+  int num10=100;
+   
+   public int add(int n1,int n2,int n3){
+ 
+    System.out.println(num10);
+     return n1+n2+n3;
+   }
+   public int add(int n1,int n2){
 
-class Computer {
-
-  public void playmusic() {
-
-    System.out.println("Plying music");
+    return n1+n2;
   }
-
-  public String givemepen(int cost) {
-
-    if (cost >= 10) {
-      return "Pen";
-    } else {
-      return "No pen";
-    }
-  }
-}
+ }
 
 public class Class {
 
   public static void main(String a[]) {
 
-    Computer play = new Computer();
-    play.playmusic();
+   
+     Calculater cale=new Calculater();
+     Calculater cale1=new Calculater();
 
-    String result = play.givemepen(1);
+      int totalnum=cale.add(1,2);
+       int totalnum2=cale1.add(10,20);
 
-    System.out.println(result);
+    System.out.println(totalnum);
+     System.out.println(totalnum2);
 
   }
 
--- Javapratice/Newhello.java
+++ Javapratice/Newhello.java
@@ -1,26 +1,52 @@
+import java.util.Arrays;
+
 public class Newhello {
-    
-     public static void main( String a[]){
 
-         int  num1=120;
-          int num2=100;
-         
-        System.out.println("NewHeelow World");
-         System.out.println(num2+num1);
+     public static void main(String a[]) {
 
-            int x=30;
-             int y=20;
-              int z=40;
+          // int num1 = 120;
+          // int num2 = 100;
 
-             if(x>y && x>z){
-                 System.out.println(x);
-            
-             }
-              else if(y>x && y>z){
-                   System.out.println(y);
-              }
-              else{
-                 System.out.println(z);
-              }
+          // System.out.println("NewHeelow World");
+          // System.out.println(num2 + num1);
+
+          // int x = 30;
+          // int y = 20;
+          // int z = 40;
+
+          // if (x > y && x > z) {
+          //      System.out.println(x);
+
+          // } else if (y > x && y > z) {
+          //      System.out.println(y);
+          // } else {
+          //      System.out.println(z);
+          // }
+
+
+
+          
+//            int nums[]=new int[4];
+//  nums[3]=5;
+//             System.out.println(Arrays.toString(nums));
+
+ int nums [] []=new int [3][4];
+
+  for(  int i=0;i<nums.length;i++){
+     for( int  j=0;j<=nums.length;j++){
+           System.out.println(nums[i][j]+" ");
      }
+     System.out.println();
+
+  }
+  
+
+
+
+
+
+
+
+     }
+
 }
